a6c8ec9f95a26a4965c5210484a27b083d947636
ljjuhang <lytran456654@gmail.com>
2019-12-24 22:18:34 +0700

    first commit

---
+++ BackEnd/ConnectionDB/test.php
@@ -0,0 +1,11 @@
+﻿<?php
+require_once('DB_classes.php');
+
+$nguoidung = new NguoiDungBUS();
+// $nguoidung->delete_by_id("ND2");
+ $nguoidung->add_new(array('ND3', 'Tran', 'Phuoc Ly', 'lytran456654@gmail.com'));
+
+show_DataBUS_as_Table(new NguoiDungBUS());
+die('LOL');
+
+// Check trùng bằng getList
